e64ce4162861f7403fac6ac4050c0cc74267389d
chenyong <baicel07@163.com>
2023-06-17 17:04:07 +0800

    fixBundle

--- src/assets/data/tabbar.js
+++ src/assets/data/tabbar.js
@@ -2,25 +2,25 @@ export const tabBarData = [
   {
     text: '首页',
     path: '/home',
-    imgUrl: '../assets/img/tabbar/tab_home.png',
-    activeImgUrl: '../assets/img/tabbar/tab_home_active.png'
+    imgUrl: 'assets/img/tabbar/tab_home.png',
+    activeImgUrl: 'assets/img/tabbar/tab_home_active.png'
   },
   {
     text: '收藏',
     path: '/favor',
-    imgUrl: '../assets/img/tabbar/tab_favor.png',
-    activeImgUrl: '../assets/img/tabbar/tab_favor_active.png'
+    imgUrl: 'assets/img/tabbar/tab_favor.png',
+    activeImgUrl: 'assets/img/tabbar/tab_favor_active.png'
   },
   {
     text: '订单',
     path: '/order',
-    imgUrl: '../assets/img/tabbar/tab_order.png',
-    activeImgUrl: '../assets/img/tabbar/tab_order_active.png'
+    imgUrl: 'assets/img/tabbar/tab_order.png',
+    activeImgUrl: 'assets/img/tabbar/tab_order_active.png'
   },
   {
     text: '消息',
     path: '/message',
-    imgUrl: '../assets/img/tabbar/tab_message.png',
-    activeImgUrl: '../assets/img/tabbar/tab_message.png'
+    imgUrl: 'assets/img/tabbar/tab_message.png',
+    activeImgUrl: 'assets/img/tabbar/tab_message.png'
   }
 ]
